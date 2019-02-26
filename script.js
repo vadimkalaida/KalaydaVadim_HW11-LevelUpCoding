8da6747ad6a8aa3556bd5d75339cc9ffda773d90
VadimKalayda <vadosgta1@gmail.com>
2019-02-26 18:26:06 +0200

    Add main loop for every level

--- script.js
+++ script.js
@@ -17,143 +17,153 @@ for (step = 0; step <= 6; step++) {
 }
 
 // fourth level
-south();
+for(step = 0; step < 1; step++) {
+    south();
 
-for(step = 0; step <= 2; step++) {
+    for(step = 0; step <= 2; step++) {
+        east();
+    }
+
+    for(step = 0; step <= 1; step++) {
+        south();
+    }
+
     east();
-}
 
-for(step = 0; step <= 1; step++) {
+    for(step = 0; step <= 2; step++) {
+        south();
+    }
+
+    for(step = 0; step <= 1; step++) {
+        east();
+    }
+
     south();
 }
-
-east();
-
-for(step = 0; step <= 2; step++) {
-    south();
-}
-
-for(step = 0; step <= 1; step++) {
-    east();
-}
-
-south();
 
 
 // fifth level
-for(step = 0; step <= 5; step++) {
-    east();
-}
+for(step = 0; step < 1; step++) {
+    for(step = 0; step <= 5; step++) {
+        east();
+    }
 
-for(step = 0; step <= 1; step++) {
-    south();
-}
+    for(step = 0; step <= 1; step++) {
+        south();
+    }
 
-for(step = 0; step <= 1; step++) {
-    west();
-}
+    for(step = 0; step <= 1; step++) {
+        west();
+    }
 
-for(step = 0; step <= 2; step++) {
-    south();
-}
+    for(step = 0; step <= 2; step++) {
+        south();
+    }
 
-for(step = 0; step <= 1; step++) {
-    west();
-}
+    for(step = 0; step <= 1; step++) {
+        west();
+    }
 
-for(step = 0; step <= 2; step++) {
-    north();
-}
+    for(step = 0; step <= 2; step++) {
+        north();
+    }
 
-for(step = 0; step <= 1; step++) {
-    west();
-}
+    for(step = 0; step <= 1; step++) {
+        west();
+    }
 
-for(step = 0; step <= 4; step++) {
-    south();
-}
+    for(step = 0; step <= 4; step++) {
+        south();
+    }
 
-for(step = 0; step <= 5; step++) {
-    east();
+    for(step = 0; step <= 5; step++) {
+        east();
+    }
+
 }
 
 
 // sixth level
-for(step = 0; step <= 4; step++) {
-    south();
-}
+for(step = 0; step < 1; step++) {
+    for(step = 0; step <= 4; step++) {
+        south();
+    }
 
-for(step = 0; step <= 1; step++) {
+    for(step = 0; step <= 1; step++) {
+        east();
+    }
+
+    for(step = 0; step <= 2; step++) {
+        north();
+    }
+
     east();
+
+    for(step = 0; step <= 1; step++) {
+        north();
+    }
+
+    for(step = 0; step <= 2; step++) {
+        east();
+    }
+
+    for(step = 0; step <= 3; step++) {
+        south();
+    } 
+
+    for(step = 0; step <= 1; step++) {
+        west();
+    }
+
+    for(step = 0; step <= 2; step++) {
+        south();
+    }
+
+    for(step = 0; step <= 1; step++) {
+        east();
+    }
 }
 
-for(step = 0; step <= 2; step++) {
-    north();
-}
-
-east();
-
-for(step = 0; step <= 1; step++) {
-    north();
-}
-
-for(step = 0; step <= 2; step++) {
-    east();
-}
-
-for(step = 0; step <= 3; step++) {
-    south();
-} 
-
-for(step = 0; step <= 1; step++) {
-    west();
-}
-
-for(step = 0; step <= 2; step++) {
-    south();
-}
-
-for(step = 0; step <= 1; step++) {
-    east();
-}
 
 // seventh level
-for(step = 0; step <= 10; step++) {
-    south();
-}
+for(step = 0; step < 1; step++) {
+    for(step = 0; step <= 10; step++) {
+        south();
+    }
 
-for(step = 0; step <= 3; step++) {
+    for(step = 0; step <= 3; step++) {
+        east();
+    }
+
+    for(step = 0; step <= 10; step++) {
+        north();
+    }
+
+    for(step = 0; step <= 1; step++) {
+        east();
+    }
+
+    south();
+
+    east();
+
+    for(step = 0; step <= 4; step++) {
+        south();
+    }
+
+    west();
+
+    for(step = 0; step <= 6; step++) {
+        south();
+    }
+
+    for(step = 0; step <= 7; step++) {
+        east();
+    }
+
+    for(step = 0; step <= 8; step++) {
+        south();
+    }
+
     east();
 }
-
-for(step = 0; step <= 10; step++) {
-    north();
-}
-
-for(step = 0; step <= 1; step++) {
-    east();
-}
-
-south();
-
-east();
-
-for(step = 0; step <= 4; step++) {
-    south();
-}
-
-west();
-
-for(step = 0; step <= 6; step++) {
-    south();
-}
-
-for(step = 0; step <= 7; step++) {
-    east();
-}
-
-for(step = 0; step <= 8; step++) {
-    south();
-}
-
-east();
